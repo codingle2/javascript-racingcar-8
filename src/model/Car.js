bdd1258ff0954bcadb3fd0180d97b019451a7292
kpss0337 <kpss0337@gmail.com>
2025-10-26 07:15:19 +0900

    feat(model): 자동차 및 레이스 클래스 구현 - 개별 차량의 움직임과 위치를 처리하기 위해 Car.js를 추가 - 여러 대의 차량을 관리 및 경주 라운드를 진행 위해 Race.js를 추가

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,5 +1,26 @@
+import { RandomUtils } from '../utils/RandomUtils.js';
+
 export class Car {
+  static MOVE_THRESHOLD = 4;
+
   constructor(name) {
     this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    const randomNumber = RandomUtils.pick();
+
+    if (randomNumber >= Car.MOVE_THRESHOLD) {
+      this.position += 1;
+    }
+  }
+
+  get positionValue() {
+    return this.position;
+  }
+
+  get carName() {
+    return this.name;
   }
 }
--- src/model/Race.js
+++ src/model/Race.js
@@ -1,6 +1,29 @@
+import { Car } from './Car.js';
+
 export class Race {
   constructor(carNames, tryCount) {
-    this.cars = [];
+    this.cars = carNames.map((name) => new Car(name));
     this.tryCount = tryCount;
+  }
+
+  playRound() {
+    this.cars.forEach((car) => {
+      car.move();
+    });
+  }
+
+  playAllRounds() {
+    Array.from({ length: this.tryCount }).forEach(() => {
+      this.playRound();
+    });
+  }
+
+  getWinners() {
+    const positions = this.cars.map((car) => car.getPosition());
+    const maxPosition = Math.max(...positions);
+
+    return this.cars
+      .filter((car) => car.getPosition() === maxPosition)
+      .map((car) => car.getName());
   }
 }
